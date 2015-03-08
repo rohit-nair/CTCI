14f869926449ea6b3661c660c3e06884fb968e7b
Rair <ronair@gmail.com>
2015-03-08 18:07:04 -0500

    Implementation for Peek(Index) for stack of stacks.

--- 03-StacksAndQueues/03-StackOfStacks-v2.cpp
+++ 03-StacksAndQueues/03-StackOfStacks-v2.cpp
@@ -50,8 +50,7 @@ class Stack {
             this->topStack = new Stack();
         }
         this->topStack->PushItem(d);
-        if (++this->currentStackItemCounter % MAX_CAPACITY == 0 &&
-            this->currentStackItemCounter != 0) {
+        if (++this->currentStackItemCounter % MAX_CAPACITY == 9) {
             Stack* newStack = new Stack();
             newStack->nextStack = this->topStack;
             this->topStack = newStack;
@@ -62,8 +61,7 @@ class Stack {
         if (this->currentStackItemCounter == -1) return -1;
         
         int valToReturn = topStack->PopItem();
-        if (--this->currentStackItemCounter % MAX_CAPACITY == 0 &&
-            this->currentStackItemCounter != 0) {
+        if (--this->currentStackItemCounter % MAX_CAPACITY == 9) {
             Stack* toDelete = this->topStack;
             this->topStack = toDelete->nextStack;
             delete toDelete;
@@ -75,20 +73,39 @@ class Stack {
     int Peek() {
         return topStack->PeekItem();
     }
+
+    int Peek(int index) {
+        int stackNumber = this->currentStackItemCounter / MAX_CAPACITY - index / MAX_CAPACITY;
+        int idxOnStack = (stackNumber == 0 ? this->currentStackItemCounter % MAX_CAPACITY : MAX_CAPACITY - 1) - (index % MAX_CAPACITY);
+        Stack* currentStack = this->topStack;
+        Item* currentItem = nullptr;
+
+        for (int i = 0; i < stackNumber; i++) {
+            currentStack = currentStack->nextStack;
+        }
+
+        currentItem = currentStack->topItem;
+        for (int j = 0; j < idxOnStack; j++) {
+            currentItem = currentItem->next;
+        }
+        return currentItem->value;
+    }
 };
 
 
 int main() {
-    int itemsOnStack = 105;
+    int itemsOnStack = 15;
     Stack stack;
     
     srand(time(0));
     for (int i = 0; i < itemsOnStack; i++) {
         int item = rand() % 10000;
-        stack.Push(item);
-        
         printf("%d Pushing %d to stack.\n", i, item);
+        stack.Push(item);
     }
+
+    int indexToPeek = rand() % itemsOnStack - 1;
+    printf("Item at %d is %d.\n", indexToPeek, stack.Peek(indexToPeek));
     
     std::string a;
     int res1 = 0;
